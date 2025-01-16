0300a9df75635c859de5dfd12ad7522421293a43
Elias Alberto Alegre Barazorda <betoab728@gmail.com>
2025-01-16 00:47:03 -0500

    se implemento slider alternando imagenes para pc y movil

--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -1,38 +1,37 @@
-// Importaciones necesarias
 "use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Image from "next/image";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination,Autoplay } from 'swiper/modules'; // Importación desde 'swiper/modules'
-import Image from 'next/image';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-// Definir los props del componente
 interface SliderProps {
-  images: { src: string; alt: string }[]; // Lista de rutas y descripciones de las imágenes
+  images: { src: string; alt: string }[];
 }
 
 const Slider: React.FC<SliderProps> = ({ images }) => {
   return (
     <div className="slider-container">
       <Swiper
-        modules={[Navigation, Pagination, Autoplay ]} // Activa los módulos de Swiper
-        navigation // Habilita la navegación
-        pagination={{ clickable: true }} // Habilita la paginación interactiva
+        modules={[Navigation, Pagination, Autoplay]}
+        navigation
+        pagination={{ clickable: true }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
-        loop // Habilita el loop continuo
+        loop
         className="w-full"
       >
         {images.map((img, index) => (
           <SwiperSlide key={index}>
-            <div className="relative w-full h-[583px]">
+            <div className="relative w-full">
               <Image
-                src={img.src} // Ruta de la imagen
-                alt={img.alt} // Texto alternativo
-                layout="fill" // Ocupa todo el contenedor
-                objectFit="cover" // Ajusta el recorte de la imagen
-                priority={index === 0} // Prioriza la carga de la primera imagen
+                src={img.src}
+                alt={img.alt}
+                layout="intrinsic" // Preserva la proporción de la imagen
+                width={1600} 
+                height={533} 
+                priority={index === 0}
+                className="object-contain" // Asegura que la imagen se ajuste sin recorte
               />
             </div>
           </SwiperSlide>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,53 @@
+"use client";
+
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import { Poppins } from "next/font/google";
 import Slider from "./components/Slider";
+import { useEffect, useState } from "react";
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['300', '400', '600', '700'],
 });
 
-// Lista de imágenes para el Slider
-const sliderImages = [
-  { src: "/img/promo4.jpg", alt: "Imagen 1" },
-  { src: "/img/promo2.png", alt: "Imagen 2" },
-  { src: "/img/promo3.png", alt: "Imagen 3" },
+
+// Listas de imágenes para PC y móvil
+const desktopImages = [
+  { src: "/img/promo5.jpg", alt: "Imagen PC 1" },
+  { src: "/img/promo6.jpg", alt: "Imagen PC 2" },
 ];
 
+const mobileImages = [
+  { src: "/img/promo5v.jpg", alt: "Imagen móvil 1" },
+  { src: "/img/promo6v.jpg", alt: "Imagen móvil 2" },
+];
+
+
 export default function Home() {
+
+  const [sliderImages, setSliderImages] = useState(desktopImages);
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Cambiar la lista de imágenes según el tamaño de la pantalla
+      if (window.matchMedia("(max-width: 768px)").matches) {
+        setSliderImages(mobileImages); // Pantalla móvil
+      } else {
+        setSliderImages(desktopImages); // Pantalla de escritorio
+      }
+    };
+     // Detectar el tamaño inicial
+     handleResize();
+
+
+     // Agregar un event listener para cambios en el tamaño
+    window.addEventListener("resize", handleResize);
+
+    // Limpiar el event listener al desmontar
+    return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
   return (
     <div className={poppins.className}>
       <Header />
